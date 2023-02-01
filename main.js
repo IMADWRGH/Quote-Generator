c86f97beace27e457e0f75f674a5bfecfb9708a6
Imad Asserne <imad.asserne@gmail.com>
2023-02-01 08:22:32 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+// document.querySelector /addEventListener/Math Object / innerText
+
+// variable
+let btn = document.querySelector('#new-quote');
+let quote = document.querySelector('.quote');
+let person = document.querySelector('.person');
+
+function randomQuote() {
+    fetch('https://api.quotable.io/random')
+        .then(response => response.json())
+        .then(data => {
+            quote.textContent = data.content;
+            person.textContent = `--${data.author}`;
+            console.log(data.json);
+        });
+}
+
+randomQuote();
+btn.addEventListener('click', () => {
+    randomQuote();
+});
