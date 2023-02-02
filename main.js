d9e235b398ac828124b0fbc05426944f15006243
Imad Asserne <imad.asserne@gmail.com>
2023-02-02 02:33:31 +0000

    add & remove commotaire

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-// document.querySelector /addEventListener/Math Object / innerText
-
 // variable
 let btn = document.querySelector('#new-quote');
 let quote = document.querySelector('.quote');
 let person = document.querySelector('.person');
 
+
+// function 
 function randomQuote() {
     fetch('https://api.quotable.io/random')
         .then(response => response.json())
         .then(data => {
             quote.textContent = `"${data.content}"`;
             person.textContent = `${data.author}`;
-            console.log(data.json);
+            //console.log(data.json);
         });
 }
 
